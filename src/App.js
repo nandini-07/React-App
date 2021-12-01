d230656a90863231b54413c6f6d84ba51a227f48
Nandini <Nandini.Kanungo@ibm.com>
2021-12-01 14:57:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import DataForm from "./components/DataForm";
+import "./components/DataForm.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <BrowserRouter>
+          <div className="navbar_main">
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark navbar-dark">
+              <h3 className="navbar-brand ml-5"> Learner's Hub</h3>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <div>
+                      <Link
+                        to="/React-App"
+                        className="nav-link"
+                        className="navbar-brand"
+                      >
+                        Home
+                      </Link>
+                    </div>
+                  </li>
+                  <li className="nav-item active">
+                    <div>
+                      <Link
+                        to="/contact"
+                        className="nav-link"
+                        className="navbar-brand"
+                      >
+                        Contact
+                      </Link>
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <div>
+                      <Link
+                        to="/details"
+                        className="nav-link"
+                        className="navbar-brand"
+                      >
+                        Details
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/React-App" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/details" element={<DataForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
